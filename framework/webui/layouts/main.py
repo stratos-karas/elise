d557c6817a99ed5229a237b76f3b2892320b32e1
Stratos Karas <stratosex777@gmail.com>
2025-04-08 01:34:39 +0300

    1. Scheduler: Adding, Removing, Uploading 2. Auto-indexing mechanism that can be generalized for the other components (workloads, actions) 3. Auto-loading mechanism for schematic store

--- framework/webui/layouts/main.py
+++ framework/webui/layouts/main.py
@@ -21,11 +21,14 @@ def create_schem_component(name: str, description: str = "", items: list[dbc.Lis
     
     btn_label = "Add " + name
     btn_id = "add-" + name.lower()
-    items.append(
+    title_div.children.append(
         dbc.ListGroupItem(
             dbc.Button(btn_label, id=btn_id, style=dict(margin=0, padding=0, width="100%")),
             style=dict(margin=0, padding=0))
-        )
+        
+    )
+    # items.append(
+    #     )
 
     component_id = "component-" + name.lower()
     component_items_id = "component-" + name.lower() + "-items"
@@ -33,9 +36,6 @@ def create_schem_component(name: str, description: str = "", items: list[dbc.Lis
     # TODO: Add a button in the title div element and make the listgroup collapsable
     return dbc.ListGroupItem([title_div, dbc.ListGroup(items, id=component_items_id)], id=component_id, action=True)
     
-def workload_item_modal_body(data=None):
-    pass
-        
 
 tools = [
     create_schem_component(name="Workloads", description="Add workloads: source, heatmap, generator, cluster", items=[]),
@@ -61,64 +61,62 @@ component_input = [Input("add-"+name.lower(), "n_clicks") for name in component_
 
 component_state = [State("component-"+name.lower()+"-items", "children") for name in component_names]
 
+def get_item_id():
+    pass
+
 @callback(
     component_ouput,
     Input("app-sim-schematic", "data"),
     component_state
 )
 def modify_component_items(data, workloads_children, schedulers_children, actions_children):
-    print(data)
-    new_workloads_children = [dbc.ListGroupItem(f"workload-{idx}") for idx, _ in enumerate(data["workloads"].keys())]
-    new_workloads_children.append(workloads_children[-1])
-    new_schedulers_children = [dbc.ListGroupItem(f"scheduler-{idx}") for idx, _ in enumerate(data["schedulers"].keys())]
-    new_schedulers_children.append(schedulers_children[-1])
-    new_actions_children = [dbc.ListGroupItem(f"action-{idx}") for idx, _ in enumerate(data["actions"].keys())]
-    new_actions_children.append(actions_children[-1])
+
+    if not data:
+        raise PreventUpdate
+
+    # new_workloads_children = [dbc.ListGroupItem(f"workload-{idx}") for idx, _ in enumerate(data["workloads"].keys())]
+    # new_workloads_children = list()
+    # for i, key in enumerate(data["workloads"].keys()):
+    #     new_workloads_children.append(
+    #         dbc.ListGroupItem(dbc.Button(key, id=dict(item="workload", index=i), style=dict(margin=0, padding=0, width="100%")),
+    #                           style=dict(margin=0, padding=0))
+    #     )
+
     
-    return new_workloads_children, new_schedulers_children, new_actions_children
+    new_schedulers_children = list()
+    for key in data["schedulers"].keys():
+        item, index = key.split('-')
+        id = {"item": item, "index": index}
+        del_id = {"delete-item": item, "index": index}
+        child_exists = False
+        
+        for item in schedulers_children:
+            btn = item["props"]["children"]["props"]["children"][0]
+            btn_id = btn["props"]["id"]
 
+            try:
+                if id == btn_id:
+                    new_schedulers_children.append(item)
+                    child_exists = True
+                    break
+            except:
+                continue
+        
+        if not child_exists:
+            new_schedulers_children.append(
+                dbc.ListGroupItem(
+                    dbc.InputGroup([
+                        dbc.Button(data["schedulers"][key]["name"], id=id, color="secondary", outline=True, size="sm", style=dict(margin=0, padding=0, width="80%")),
+                        dbc.Button(html.I(className="bi bi-x"), id=del_id, size="sm", style=dict(width="20%"))
+                    ])
+                , style=dict(margin=0, padding=0)),
+            )
 
+    # new_actions_children = list()
+    # for i, key in enumerate(data["actions"].keys()):
+    #     new_actions_children.append(
+    #         dbc.ListGroupItem(dbc.Button(key, id=dict(item="action", index=i), style=dict(margin=0, padding=0, width="100%")),
+    #                           style=dict(margin=0, padding=0))
+    #     )
 
-# @callback(
-#     component_ouput,
-#     component_input,
-#     component_state
-# )
-# def modify_component_list(btn_workloads, btn_schedulers, btn_actions, children_workloads, children_schedulers, children_actions, app_schematic_data):
-#     print(callback_context.triggered_id)
-#     match callback_context.triggered_id:
-#         case "add-workloads":
-#             workloads_len = len(app_schematic_data["workloads"].keys())
-#             workload_id = f"workload-{workloads_len - 1}"
-
-#             new_items = children_workloads[:-1]
-#             new_items.append(dbc.ListGroupItem(workload_id, id=workload_id))
-#             new_items.append(children_workloads[-1])
-            
-#             app_schematic_data["workloads"].update({workload_id: None})
-
-#             return new_items, children_schedulers, children_actions, app_schematic_data
-#         case "add-schedulers":
-#             schedulers_len = len(app_schematic_data["schedulers"].keys())
-#             scheduler_id = f"workload-{schedulers_len- 1}"
-
-#             new_items = children_schedulers[:-1]
-#             new_items.append(dbc.ListGroupItem("scheduler"))
-#             new_items.append(children_schedulers[-1])
-            
-#             app_schematic_data["schedulers"].update({scheduler_id: None})
-
-#             return children_workloads, new_items, children_actions, app_schematic_data
-#         case "add-actions":
-#             actions_len = len(app_schematic_data["actions"].keys())
-#             action_id = f"workload-{actions_len- 1}"
-
-#             new_items = children_actions[:-1]
-#             new_items.append(dbc.ListGroupItem("action"))
-#             new_items.append(children_actions[-1])
-
-#             app_schematic_data["actions"].update({action_id: None})
-
-#             return children_workloads, children_schedulers, new_items, app_schematic_data
-#         case _:
-#             raise PreventUpdate
+    return workloads_children, new_schedulers_children, actions_children
--- framework/webui/utils/scheduler_utils.py
+++ framework/webui/utils/scheduler_utils.py
@@ -1,8 +1,11 @@
-from dash import html
+from dash import html, ALL
 from dash import callback, callback_context, Output, Input, State
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+
+import base64
+import tempfile
 
 
 """
@@ -11,7 +14,8 @@ app-sim-schematic::schedulers
     keyword (str): scheduler-{id}
     value:
         method (str): Defaults or Custom
-        value (str): scheduler name or file path
+        name (str): scheduler's name
+        value (str): scheduler's name or file path
         default (bool): if it is the default scheduler
         options list(int): list of the options enabled for the scheduler
 """
@@ -21,61 +25,64 @@ def scheduler_item_modal_body(data=None):
     """
     
     
-    # if data is None:
+    if data is None:
 
-    div = html.Div([
-        dbc.InputGroup([dbc.Select(["Defaults", "Custom"], "Defaults", id="scheduler-item-select-method"), 
-                        dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler", id="scheduler-item-select-scheduler", style={"display": "block"}),
-                        dcc.Upload(dbc.Button("Upload file", style=dict(width="100%")), id="scheduler-item-upload-scheduler", style={"display": "none"}),
-        ]),
-        html.Footer(id="scheduler-item-upload-footer", className="small", style=dict(display="none")),
-        dbc.Checklist(
-            id="scheduler-item-options",
-            options=[dict(label="Default", value=1), dict(label="Backfilling", value=2), dict(label="Compact fallback", value=3)],
-            value=[],
-            inline=True,
-            switch=True
-        ),
-        dbc.Button("Save Scheduler", id="scheduler-item-save-btn")
-    ])
+        div = html.Div([
+            dbc.InputGroup([dbc.Select(["Defaults", "Custom"], "Defaults", id="scheduler-item-select-method"), 
+                            dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler", id="scheduler-item-select-scheduler", style={"display": "block"}),
+                            dcc.Upload(dbc.Button("Upload file", style=dict(width="100%")), id="scheduler-item-upload-scheduler", style={"display": "none"}),
+            ]),
+            html.Footer(id="scheduler-item-upload-footer", className="small", style=dict(display="none")),
+            dbc.Checklist(
+                id="scheduler-item-options",
+                options=[dict(label="Default", value=1), dict(label="Backfilling", value=2), dict(label="Compact fallback", value=3)],
+                value=[],
+                inline=True,
+                switch=True
+            ),
+            dbc.Button("Save Scheduler", id="scheduler-item-save-btn")
+        ])
+
+    else:
+        method = data["method"]
+        value = data["name"]
+        options_values = data["options"]
+
+        if method == "Custom":
+            div = html.Div([
+                dbc.InputGroup([dbc.Select(["Defaults", "Custom"], "Custom", id="scheduler-item-select-method"), 
+                                dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler", id="scheduler-item-select-scheduler", style={"display": "none"}),
+                                dcc.Upload(dbc.Button("Upload file", style=dict(width="100%")), id="scheduler-item-upload-scheduler", style={"display": "block"}),
+                ]),
+                html.Footer(f"* Uploaded scheduler file: {value}", id="scheduler-item-upload-footer", className="small", style=dict(display="block")),
+                dbc.Checklist(
+                    id="scheduler-item-options",
+                    options=[dict(label="Default", value=1), dict(label="Backfilling", value=2), dict(label="Compact fallback", value=3)],
+                    value=options_values,
+                    inline=True,
+                    switch=True
+                ),
+                dbc.Button("Save Scheduler", id="scheduler-item-save-btn")
+            ])
+        else:
+            div = html.Div([
+                dbc.InputGroup([dbc.Select(["Defaults", "Custom"], "Defaults", id="scheduler-item-select-method"), 
+                                dbc.Select(["FIFO Scheduler", "EASY Scheduler"], value, id="scheduler-item-select-scheduler", style={"display": "block"}),
+                                dcc.Upload(dbc.Button("Upload file", style=dict(width="100%")), id="scheduler-item-upload-scheduler", style={"display": "none"}),
+                ]),
+                html.Footer(id="scheduler-item-upload-footer", className="small", style=dict(display="none")),
+                dbc.Checklist(
+                    id="scheduler-item-options",
+                    options=[dict(label="Default", value=1), dict(label="Backfilling", value=2), dict(label="Compact fallback", value=3)],
+                    value=options_values,
+                    inline=True,
+                    switch=True
+                ),
+                dbc.Button("Save Scheduler", id="scheduler-item-save-btn")
+            ])
     
     return div
-    # else:
-    #     method, value = list(data["method"].items())
-    #     match method:
-    #         case "file":
-    #             select_scheduler_element = dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler", style=hidden_style)
-    #             upload_scheduler_element = dbc.Button("Upload file", style=displayed_style)
-    #         case _:
-    #             select_scheduler_element = dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler", style=displayed_style)
-    #             upload_scheduler_element = dbc.Button("Upload file", style=hidden_style)
-
-    #     options_enabled = data["options"]
-
-    #     div = html.Div([
-    #         dbc.InputGroup(
-    #             [
-    #                 dbc.Select(["Defaults", "Custom"], "Defaults", id="scheduler-item-select-method"), 
-    #                 dbc.Select(["FIFO Scheduler", "EASY Scheduler"], "FIFO Scheduler"), 
-    #                 dbc.Button("Upload file")
-    #             ], id="scheduler-item-methodinput"
-    #         ),
-    #         dbc.Checklist(
-    #             options=[dict(label="Backfilling", value=1), dict(label="Compact fallback", value=2)],
-    #             value=options_enabled,
-    #             inline=True,
-    #             switch=True
-    #         ),
-    #         dbc.Button("Save")
-    #     ])
-
-# @callback(
-#     Output("app-sim-schematic", "data")
-#     Input("schedulers-modal-save-btn", "n_clicks")
-# )
-# def schedulers_modal_save_button(n_clicks):
-#     pass
-    
+ 
 schedulers_modal = dbc.Modal([
     dbc.ModalHeader(dbc.ModalTitle("Schedulers")),
     dbc.ModalBody(id="schedulers-modal-body")
@@ -85,18 +92,32 @@ schedulers_modal = dbc.Modal([
 @callback(
    Output("schedulers-modal", "is_open"),
    Output("schedulers-modal-body", "children"),
+   Output("add-schedulers", "n_clicks"),
+   Output({"item": "scheduler", "index": ALL}, "n_clicks"),
    Input("add-schedulers", "n_clicks"),
+   Input({"item": "scheduler", "index": ALL}, "n_clicks"),
+   State("app-sim-schematic", "data"),
+   prevent_initial_call=True,
 )
-def schedulers_modal_open(n_clicks):
-    if n_clicks is None:
+def schedulers_modal_open(n_clicks, m_clicks, data):
+    
+    if not n_clicks and not any(m_clicks):
         raise PreventUpdate
-    return True, scheduler_item_modal_body()
+
+    trigger_id = callback_context.triggered_id
+    print(trigger_id)
+    if trigger_id == "add-schedulers":
+        return True, scheduler_item_modal_body(), None, [None] * len(m_clicks)
+    else:
+        idx = int(trigger_id["index"])
+        return True, scheduler_item_modal_body(data["schedulers"][f"scheduler-{idx}"]), None, [None] * len(m_clicks)
 
 @callback(
     Output("scheduler-item-select-scheduler", "style"),
     Output("scheduler-item-upload-scheduler", "style"),
     Output("scheduler-item-upload-footer", "style"),
     Input("scheduler-item-select-method", "value"),
+    prevent_initial_call=True
 )
 def scheduler_item_select_method(value):
     if not value:
@@ -111,12 +132,22 @@ def scheduler_item_select_method(value):
 
 @callback(
     Output("scheduler-item-upload-footer", "children"),
-    Input("scheduler-item-upload-scheduler", "filename")
+    Input("scheduler-item-upload-scheduler", "filename"),
+    prevent_initial_call=True
 )
 def scheduler_item_upload_scheduler(filename):
-    if not filename:
-        raise PreventUpdate
     return f"* Uploaded scheduler file: {filename}"
+
+
+def parse_uploaded_scheduler_contents(enc_contents):
+    # Decode the contents
+    content_type, content_string = enc_contents.split(',')
+    
+    if "python" not in content_type.lower():
+        return ""
+
+    decoded = base64.b64decode(content_string)
+    return decoded.decode('utf-8')
 
 @callback(
     Output("app-sim-schematic", "data"),
@@ -124,22 +155,72 @@ def scheduler_item_upload_scheduler(filename):
     State("scheduler-item-select-method", "value"),
     State("scheduler-item-select-scheduler", "value"),
     State("scheduler-item-upload-scheduler", "filename"),
+    State("scheduler-item-upload-scheduler", "contents"),
     State("scheduler-item-options", "value"),
-    State("app-sim-schematic", "data")
+    State("app-sim-schematic", "data"),
+    prevent_initial_call=True,
 )
-def save_scheduler_item(n_clicks, method, select_scheduler_value, upload_scheduler_value, options, schematic_data):
+def save_scheduler_item(n_clicks, 
+                        method, 
+                        select_scheduler_value, 
+                        upload_scheduler_value, 
+                        upload_scheduler_contents, 
+                        options, 
+                        schematic_data):
     if not n_clicks:
         raise PreventUpdate
-
     data = dict()
     data["method"] = method
     data["options"] = options
     if method == "Custom":
-        data["value"] = upload_scheduler_value
+        dec_contents = parse_uploaded_scheduler_contents(upload_scheduler_contents)
+        if not dec_contents:
+            # TODO: alert the user of violations
+            raise PreventUpdate
+        filename = tempfile.mktemp(suffix=".py", prefix=upload_scheduler_value)
+        with open(filename, "w") as fd:
+            fd.write(dec_contents)
+        data["value"] = filename
+        # TODO: automatic load schedulers that are created inside the schedulers' directory
+        data["name"] = upload_scheduler_value
     else:
         data["value"] = select_scheduler_value
+        data["name"] = select_scheduler_value
     
-    scheduler_id = len(list(schematic_data["schedulers"].keys())) - 1
-    schematic_data["schedulers"].update({scheduler_id: data})
+    # Find the new scheduler's id based on the already existing ids
+    stored_scheduler_ids = set(
+        [int(keyval.split('-')[1]) for keyval in schematic_data["schedulers"].keys()]
+    )
+    max_val = -1
+    if stored_scheduler_ids:
+        max_val = max(stored_scheduler_ids)
+        possible_scheduler_ids = set(range(0, max_val+1))
+        diff = possible_scheduler_ids - stored_scheduler_ids
+        # If there are unused ids inside the range of the current 0 and max then use them
+        if diff:
+            scheduler_id = diff.pop()
+        else:
+            scheduler_id = max_val + 1
+    else:
+        scheduler_id = max_val + 1
+
+    
+    schematic_data["schedulers"].update({f"scheduler-{scheduler_id}": data})
+    
+    return schematic_data
+
+@callback(
+    Output("app-sim-schematic", "data", allow_duplicate=True),
+    Input({"delete-item": "scheduler", "index": ALL}, "n_clicks"),
+    State("app-sim-schematic", "data"),
+    prevent_initial_call=True,
+)
+def remove_scheduler_item(n_clicks, schematic_data):
+    if not any(n_clicks):
+        raise PreventUpdate
+    
+    triggered_id = callback_context.triggered_id
+    index = triggered_id["index"]
+    schematic_data["schedulers"].pop(f"scheduler-{index}")
     
     return schematic_data
