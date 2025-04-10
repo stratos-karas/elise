512ed2fe856507b69e0629a665056a3a1af38096
Stratos Karas <stratosex777@gmail.com>
2025-04-10 23:09:38 +0300

    1. Inputs Component: Adding, Removing and Modifying inputs for the simulation schematic 2. Created cryptographic mechanism for uploaded filenames

--- framework/webui/layouts/main.py
+++ framework/webui/layouts/main.py
@@ -1,10 +1,10 @@
-from dash import html
+from dash import dcc, html
 from dash import callback, callback_context, Output, Input, State
 from dash.exceptions import PreventUpdate
 # import dash_blueprint_components as dpc
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 
+from utils.input_utils import inputs_modal
 from utils.scheduler_utils import schedulers_modal
 
 def create_schem_component(name: str, description: str = "", items: list[dbc.ListGroupItem] = list()):
@@ -13,34 +13,30 @@ def create_schem_component(name: str, description: str = "", items: list[dbc.Lis
     + description: description of the component
     + items: items of the component
     """
+
     title = html.H3(name)
-    title_div = html.Div([title], id=f"tooltip-{name.lower()}-target")
+
+    btn_label = html.I(className="bi bi-plus-square")
+    btn_id = "add-" + name.lower()
+    btn = dbc.Button(btn_label, id=btn_id, className="ml-auto", size="sm")
+
+    title_div = html.Div([title, btn], id=f"tooltip-{name.lower()}-target", className="d-flex align-items-center justify-content-between")
     
     if description:
         title_div.children.append(dbc.Tooltip(description, delay=dict(show=1000, hide=0), target=f"tooltip-{name.lower()}-target"))
-    
-    btn_label = "Add " + name
-    btn_id = "add-" + name.lower()
-    title_div.children.append(
-        dbc.ListGroupItem(
-            dbc.Button(btn_label, id=btn_id, style=dict(margin=0, padding=0, width="100%")),
-            style=dict(margin=0, padding=0))
-        
-    )
-    # items.append(
-    #     )
 
     component_id = "component-" + name.lower()
     component_items_id = "component-" + name.lower() + "-items"
     
     # TODO: Add a button in the title div element and make the listgroup collapsable
-    return dbc.ListGroupItem([title_div, dbc.ListGroup(items, id=component_items_id)], id=component_id, action=True)
+    return dbc.ListGroupItem([title_div, dbc.ListGroup(items, id=component_items_id)], id=component_id, action=True, style=dict(width="100%"))
     
 
 tools = [
-    create_schem_component(name="Workloads", description="Add workloads: source, heatmap, generator, cluster", items=[]),
+    create_schem_component(name="Inputs", description="Add inputs: source, heatmap, generator, cluster", items=[]),
     create_schem_component(name="Schedulers", description="Add schedulers: known or custom", items=[]),
     create_schem_component(name="Actions", description="Add actions: preprocessing and postprocessing", items=[]),
+    inputs_modal,
     schedulers_modal
 ]
 tools_ctn = dbc.ListGroup(children=tools, id="tools-container", key="tools-container", style=dict(width="100%"))
@@ -54,7 +50,7 @@ main_layout = dbc.Container([
 
 
 
-component_names = ["Workloads", "Schedulers", "Actions"]
+component_names = ["Inputs", "Schedulers", "Actions"]
 component_ouput = [Output("component-"+name.lower()+"-items", "children") for name in component_names]
 
 component_input = [Input("add-"+name.lower(), "n_clicks") for name in component_names]
@@ -69,22 +65,13 @@ def get_item_id():
     Input("app-sim-schematic", "data"),
     component_state
 )
-def modify_component_items(data, workloads_children, schedulers_children, actions_children):
+def modify_component_items(data, inputs_children, schedulers_children, actions_children):
 
     if not data:
         raise PreventUpdate
 
-    # new_workloads_children = [dbc.ListGroupItem(f"workload-{idx}") for idx, _ in enumerate(data["workloads"].keys())]
-    # new_workloads_children = list()
-    # for i, key in enumerate(data["workloads"].keys()):
-    #     new_workloads_children.append(
-    #         dbc.ListGroupItem(dbc.Button(key, id=dict(item="workload", index=i), style=dict(margin=0, padding=0, width="100%")),
-    #                           style=dict(margin=0, padding=0))
-    #     )
-
-    
-    new_schedulers_children = list()
-    for key in data["schedulers"].keys():
+    new_inputs_children = list()
+    for key, val in data["inputs"].items():
         item, index = key.split('-')
         id = {"item": item, "index": index}
         del_id = {"delete-item": item, "index": index}
@@ -93,14 +80,42 @@ def modify_component_items(data, workloads_children, schedulers_children, action
         for item in schedulers_children:
             btn = item["props"]["children"]["props"]["children"][0]
             btn_id = btn["props"]["id"]
+            btn_label = btn["props"]["children"]
 
-            try:
-                if id == btn_id:
-                    new_schedulers_children.append(item)
-                    child_exists = True
-                    break
-            except:
-                continue
+            # Check if they share the same id and name==label
+            if id == btn_id and val["name"] == btn_label:
+                new_inputs_children.append(item)
+                child_exists = True
+                break
+        
+        if not child_exists:
+            new_inputs_children.append(
+                dbc.ListGroupItem(
+                    dbc.InputGroup([
+                        dbc.Button(data["inputs"][key]["name"], id=id, color="secondary", outline=True, size="sm", style=dict(margin=0, padding=0, width="80%")),
+                        dbc.Button(html.I(className="bi bi-x"), id=del_id, size="sm", style=dict(width="20%"))
+                    ])
+                , style=dict(margin=0, padding=0)),
+            )
+
+    
+    new_schedulers_children = list()
+    for key, val in data["schedulers"].items():
+        item, index = key.split('-')
+        id = {"item": item, "index": index}
+        del_id = {"delete-item": item, "index": index}
+        child_exists = False
+        
+        for item in schedulers_children:
+            btn = item["props"]["children"]["props"]["children"][0]
+            btn_id = btn["props"]["id"]
+            btn_label = btn["props"]["children"]
+
+            # Check if they share the same id and name==label
+            if id == btn_id and val["name"] == btn_label:
+                new_schedulers_children.append(item)
+                child_exists = True
+                break
         
         if not child_exists:
             new_schedulers_children.append(
@@ -119,4 +134,4 @@ def modify_component_items(data, workloads_children, schedulers_children, action
     #                           style=dict(margin=0, padding=0))
     #     )
 
-    return workloads_children, new_schedulers_children, actions_children
+    return new_inputs_children, new_schedulers_children, actions_children
